54418cbee6dabb9f76e002543acc8ac265d8baa2
ifme18 <mainakenken188@gmail.com>
2023-08-01 12:21:51 +0000

    This a multiple school management application build by flutter and firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,19 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-admin.initializeApp();
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-// TypeScript equivalent of `_registerAdmin` function from Dart code
-export const registerAdmin = functions.https.onCall(async (data, context) => {
-  try {
-    const { email, name, schoolName, password } = data;
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 
-    // Perform your logic to create the admin account here
-    // For example, you can use the Firebase Admin SDK to create the admin user
-    const userRecord = await admin.auth().createUser({
-      email: email,
-      displayName: name,
-      password: password,
-    });
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
 
-    // Add a custom claim to the user indicating they are an admin
-    await admin.auth().setCustomUserClaims(userRecord.uid, { role: "admin" });
-
-    return { status: "success" };
-  } catch (error) {
-    console.error("Error registering:", error);
-    // Return an error message if needed
-    return { status: "error", message: "Failed to create an admin account." };
-  }
-});
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
